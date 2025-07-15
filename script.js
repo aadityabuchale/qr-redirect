0b6b04aaace2f534f44a71750fdaca5f7062c5b5
aaditya.buchale <aaditya.buchale@netenrich.com>
2025-07-15 16:18:41 +0530

    updated ui with two links, instagram and website

---
+++ script.js
@@ -0,0 +1,7 @@
+// const cardImg = document.querySelector(".card__profile");
+// const cardTooltip = document.querySelector(".card__tooltip");
+
+// cardImg.addEventListener("click", () => {
+//   cardTooltip.classList.toggle("show-tooltip");
+//   cardImg.classList.toggle("fade-profile");
+// });
